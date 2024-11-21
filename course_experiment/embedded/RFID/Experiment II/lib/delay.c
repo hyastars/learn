6f34cdb0d013e4031894f0010ee8ce888bc93acc
PC <loli@mka.cc>
2024-11-21 13:36:00 +0800

    RFID-UART-lib

---
+++ course_experiment/embedded/RFID/Experiment II/lib/delay.c
@@ -0,0 +1,9 @@
+#include <ioCC2530.h>
+#include <delay.h>
+
+void Delay_ms(unit n){
+    unit i, j;
+    for(i=0 ;i<n ;i++){
+        for(j=0 ;j<1774 ;j++)
+    }
+}
---
+++ course_experiment/embedded/RFID/Experiment II/lib/delay.h
@@ -0,0 +1,10 @@
+#ifndef _DELAY_H
+#define _DELAY_H
+
+#define uint unsigned int
+#define unit unsigned char
+
+void Delay_ms(unit n);
+
+#endif
+
