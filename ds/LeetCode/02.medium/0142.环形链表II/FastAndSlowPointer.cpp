8f136b28c441d4fded3cd1e03f54f5a6fe3e3b0e
hyastar <loli@mka.cc>
2024-12-24 22:08:11 -0500

    0142-FastAndSlowPointer

--- ds/LeetCode/02.medium/0142.环形链表II/FastAndSlowPointer.cpp
+++ ds/LeetCode/02.medium/0142.环形链表II/FastAndSlowPointer.cpp
@@ -1,13 +1,21 @@
 class Solution{
 public:
-    bool hasCycle(ListNode* head){
+    ListNode *detectCycle(ListNode *head){
         ListNode* fast = head;
         ListNode* slow = head;
         while(fast != NULL && fast->next != NULL){
             slow = slow->next;
             fast = fast->next->next;
-            if(slow = fast) return true;
+            if(slow = fast){
+                ListNode* index1 = fast;
+                ListNode* index2 = head;
+                while(index1 != index2){
+                    index1 = index1->next;
+                    index2 = index2->next;
+                }
+                return index2;
+            }
         }
-        return false;
+        return NULL;
     }
 };
